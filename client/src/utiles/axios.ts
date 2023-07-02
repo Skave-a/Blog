1ffb4481af9806afd3be7bc6281f79b204d00733
Skave-a <arrowtata@gmail.com>
2023-07-02 11:20:21 +0300

    fix: https

--- client/src/utiles/axios.ts
+++ client/src/utiles/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://localhost:3002/api",
-  baseURL: "http://blog-three-eta-22.vercel.app/api",
+  baseURL: "https://blog-three-eta-22.vercel.app/api",
 
 });
 
